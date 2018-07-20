3d1992fff5eef53ff007fd32a05750c1ac217d08
Kasinath Ravi <kasinath.ravi@SGZ.com>
2018-07-20 12:20:12 +0530

    Added Student Struct to code

--- Practice/DemoDay2.go
+++ Practice/DemoDay2.go
@@ -5,9 +5,18 @@ import (
 	"net/http"
 )
 
+//Student - Student struct
+type Student struct {
+	studentName string
+	rollNumber  int32
+	class       string
+	section     string
+}
+
 func main() {
 	http.HandleFunc("/", IndexHandler)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/student", DisplayStudent)
 	http.ListenAndServe(":8005", nil)
 }
 
@@ -20,4 +29,15 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 //About - To display About page content
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Displaying About Page")
+}
+
+//DisplayStudent - To display a Student Information
+func DisplayStudent(w http.ResponseWriter, r *http.Request) {
+	myStudent := Student{studentName: "C.R.Kasinath",
+		rollNumber: 44,
+		class:      "XII",
+		section:    "A"}
+	fmt.Fprintln(w, "The Name of Student is "+myStudent.studentName)
+	fmt.Fprintln(w, "The Class of the Student "+myStudent.studentName+" is "+myStudent.class)
+	fmt.Fprintln(w, "The Section of the Student "+myStudent.studentName+" is "+myStudent.section)
 }
